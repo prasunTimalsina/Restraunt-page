9034fede6f993a6b647b83ff9adf3a09411726b2
prasunTimalsina <timalsinaprasun61@gmail.com>
2024-11-28 15:37:29 +0545

    Add about section and load menu content
    	Completed making about section content and styling
    	and added pictures and set up grid for menu items.

---
+++ src/home.js
@@ -0,0 +1,19 @@
+import heroImage from "./imgs/home.png";
+
+export const home = `
+  <div class="home">
+        <div class="title">
+          <h2 class="title-heading">
+            Welcome to Baratie Where Your Hunger Meets the Pirate's Platter!
+          </h2>
+          <p>Fresh Seafood, Fiery Chefs, and a Side of Swashbuckling! 🏴‍☠️🍴</p>
+          <div class="hero-btns">
+            <div class="hero-btn hero-ctc">Book a Table</div>
+            <div class="hero-btn explore-btn">Explore</div>
+          </div>
+        </div>
+        <div class="hero-img-holder">
+          <img class="hero-img" src="${heroImage}" alt="" />
+        </div>
+      </div>
+`;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import "./style.css";
+import { home } from "./home";
+import { about } from "./about";
 import html from "./template.html";
 
 const content = document.querySelector(".content");
+console.log(home);
+/* content.insertAdjacentHTML("afterend", home); */
+/* content.insertAdjacentHTML("afterend", about); */
